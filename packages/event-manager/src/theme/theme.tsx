dc7b8f45a4ae258a02e14dc9098118d36fc63d75
Daan Rosbergen <d.rosbergen@gmail.com>
2021-06-10 12:07:35 +0200

    add roboto as default font

--- packages/event-manager/src/theme/theme.tsx
+++ packages/event-manager/src/theme/theme.tsx
@@ -22,7 +22,7 @@ const defaultTheme: DefaultTheme = {
   },
   font: {
     size: 16,
-    family: "'Noto Sans', sans-serif",
+    family: "'Roboto', sans-serif",
   },
   effects: {
     // @todo: this color is not dynamic, boxShadowPrimary should be a function composing shadow size (don't think that should be configurable) and color together
