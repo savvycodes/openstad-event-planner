8bcbbdf6569aedb2c81087331c58c8cfb0a5afaa
Sijmen Jerom Kok <sijmenjeromkok@me.com>
2022-06-16 14:55:19 +0200

    styled my activities

--- packages/event-favorites/src/components/emptyState/emptyState.tsx
+++ packages/event-favorites/src/components/emptyState/emptyState.tsx
@@ -1,13 +1,10 @@
-import { Paragraph, Title } from '../text/text';
 import { styled } from 'goober';
 import React from 'react';
 
 import { Heart } from 'react-feather';
-import { RedButtonLink } from '../button/button';
 import { useConfig } from '../../context/config-context';
 
 const EmptyStateContainer = styled('div')`
-  background: ${props => props.theme.colors.background};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -19,25 +16,26 @@ const EmptyStateContentWrapper = styled('div')`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 40%;
   padding: 48px;
 `;
 
 export function EmptyState() {
   const { activityPageUrl } = useConfig();
   return (
-    <EmptyStateContainer>
-      <EmptyStateContentWrapper>
+    <EmptyStateContainer className="empty-state-container">
+      <EmptyStateContentWrapper className="empty-state-content-wrapper">
         <Heart fill="#EC0000" stroke="#EC0000" width="64" height="64" />
-        <Title>Geen favorieten activiteiten</Title>
-        <Paragraph>
+        <h3>Geen favorieten activiteiten</h3>
+        <p>
           Je kan activiteiten waar je graag aan wilt deelnemen hier verzamelen
           door op het hartje te klikken
-        </Paragraph>
+        </p>
 
-        <RedButtonLink href={activityPageUrl}>
-          Alle activiteiten bekijken
-        </RedButtonLink>
+        <a href={activityPageUrl}>
+          <button>
+            Alle activiteiten bekijken
+          </button>
+        </a>
       </EmptyStateContentWrapper>
     </EmptyStateContainer>
   );
